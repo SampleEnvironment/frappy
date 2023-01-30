4fe5ddeaa77c6def803a6de30a69854f0461c481
Markus Zolliker <markus.zolliker@psi.ch>
2023-01-30 14:39:33 +0100

    demo lakeshore with simulation
    - example for lakeshore tutorial
    - lakeshore simulator (merge with frappy_psils370sim)
    - rename stringio-server to sim-server
    
    Change-Id: I33a9c75ea268349573f8a8387910921e19f242eb
    Reviewed-on: https://forge.frm2.tum.de/review/c/secop/frappy/+/30516
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Markus Zolliker <markus.zolliker@psi.ch>

--- frappy_demo/lscsim.py
+++ frappy_demo/lscsim.py
@@ -18,7 +18,11 @@
 # Module authors:
 #   Markus Zolliker <markus.zolliker@psi.ch>
 # *****************************************************************************
-"""a very simple simulator for a LakeShore Model 370"""
+"""a very simple simulator for a LakeShore
+
+Model 370: parameters are stored, but no cryo simulation
+Model 336: heat exchanger on channel A with loop 1, sample sensor on channel B
+"""
 
 from frappy.modules import Communicator
 
@@ -36,40 +40,88 @@ class Ls370Sim(Communicator):
         ('SCAN?', '3,1'),
         ('*OPC?', '1'),
     ]
+    pollinterval = 1
+
+    CHANNELS = list(range(1, 17))
+    data = ()
 
     def earlyInit(self):
         super().earlyInit()
-        self._data = dict(self.OTHER_COMMANDS)
+        self.data = dict(self.OTHER_COMMANDS)
         for fmt, v in self.CHANNEL_COMMANDS:
-            for chan in range(1,17):
-                self._data[fmt % chan] = v
+            for chan in self.CHANNELS:
+                self.data[fmt % chan] = v
 
-    def communicate(self, command):
-        self.comLog('> %s' % command)
-        # simulation part, time independent
-        for channel in range(1,17):
+    def doPoll(self):
+        super().doPoll()
+        self.simulate()
+
+    def simulate(self):
+        # not really a simulation. just for testing RDGST
+        for channel in self.CHANNELS:
             _, _, _, _, excoff = self._data['RDGRNG?%d' % channel].split(',')
             if excoff == '1':
                 self._data['RDGST?%d' % channel] = '6'
             else:
                 self._data['RDGST?%d' % channel] = '0'
 
+    def communicate(self, command):
+        self.comLog('> %s' % command)
+
         chunks = command.split(';')
         reply = []
         for chunk in chunks:
             if '?' in chunk:
-                reply.append(self._data[chunk])
+                chunk = chunk.replace('? ', '?')
+                reply.append(self.data[chunk])
             else:
-                for nqarg in (1,0):
+                for nqarg in (1, 0):
                     if nqarg == 0:
                         qcmd, arg = chunk.split(' ', 1)
                         qcmd += '?'
                     else:
                         qcmd, arg = chunk.split(',', nqarg)
                         qcmd = qcmd.replace(' ', '?', 1)
-                    if qcmd in self._data:
-                        self._data[qcmd] = arg
+                    if qcmd in self.data:
+                        self.data[qcmd] = arg
                         break
         reply = ';'.join(reply)
         self.comLog('< %s' % reply)
         return reply
+
+
+class Ls336Sim(Ls370Sim):
+    CHANNEL_COMMANDS = [
+        ('KRDG?%s', '295.0'),
+        ('RDGST?%s', '0'),
+    ]
+    OTHER_COMMANDS = [
+        ('*IDN?', 'LSCI,MODEL370,370184,05302003'),
+        ('RANGE?1', '0'),
+        ('SETP?1', '0'),
+        ('CLIMIT?1', ''),
+        ('CSET?1', ''),
+        ('CMODE?1', ''),
+        ('*OPC?', '1'),
+    ]
+
+    CHANNELS = 'ABCD'
+
+    vti = 295
+    sample = 295
+
+    def simulate(self):
+        # simple temperature control on channel A:
+        range_ = int(self.data['RANGE?1'])
+        setp = float(self.data['SETP?1'])
+        if range_:
+            # heater on: approach setpoint with 20 sec time constant
+            self.vti = max(self.vti - 0.1, self.vti + (setp - self.vti) * 0.05)
+        else:
+            # heater off 0.1/sec cool down
+            self.vti = max(1.5, self.vti - 0.1)
+        # sample approaching setpoint with 10 sec time constant, but with some
+        # systematic heat loss towards 150 K
+        self.sample = self.sample + (self.vti + (150 - self.vti) * 0.01 - self.sample) * 0.1
+        self.data['KRDG?A'] = str(round(self.vti, 3))
+        self.data['KRDG?B'] = str(round(self.sample, 3))
