d50718a1215290e9048267a122b055b0d3c5aeb0
Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
2024-10-16 13:20:24 +0200

    add test for importing custom modules
    related: #4882
    
    Change-Id: I605de7fd43be146c84f4324d2a8eebe0aa11da79
    Reviewed-on: https://forge.frm2.tum.de/review/c/secop/frappy/+/34814
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Tested-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Markus Zolliker <markus.zolliker@psi.ch>

--- test/test_test.py
+++ test/test_test.py
@@ -1,4 +1,5 @@
 
+from frappy.gui.cfg_editor.utils import get_modules
 
 def test_assert():
     assert 1
@@ -7,3 +8,7 @@ def test_assert():
 def test_constants(constants):
     assert constants.ONE == 1
     assert constants.TWO == 2
+
+
+def test_imports():
+    get_modules()
